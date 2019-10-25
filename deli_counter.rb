addda89840223711da73d8268e2fcbd20d574df9
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-25 03:43:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,3 @@
-require 'pry'
-
 def line(katz_deli)
-  deli_line = katz_deli.length 
-  if deli_line > 0 
-    deli_line.each do |num, name|
-      puts "The line is currently: #{num} #{deli_line}"
-    end 
-  puts current_line
-  else
-    puts "The line is currently empty."
-  end 
+  
 end
