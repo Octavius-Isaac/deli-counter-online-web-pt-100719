e18af0bc7bb7aa3919abb2eeeec6cdbfd9490f7a
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-25 03:27:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ require 'pry'
 def line(katz_deli)
   deli_line = katz_deli.length 
   if deli_line > 0  
-    deli_line.each { |n| puts "Current number is: #{n}" }
+    deli_line.each { |n, | puts "Current number is: #{n}" }
     end 
   puts current_line
   else
