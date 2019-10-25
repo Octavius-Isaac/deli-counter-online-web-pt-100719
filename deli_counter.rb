c723bc1e74587df1fe3ab94c61ab311e575dc60e
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-25 03:41:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,6 @@ def line(katz_deli)
   deli_line = katz_deli.length 
   if deli_line > 0 
     deli_line.each do |num, name|
-      num = 1 
       puts "The line is currently: #{num} #{deli_line}"
     end 
   puts current_line
