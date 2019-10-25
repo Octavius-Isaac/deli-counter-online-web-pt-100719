442b8385cf80c6563bde6aa26be3c85af09c07e4
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-25 03:16:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+require 'pry'
+
+def line(katz_deli)
+  deli_line = katz_deli.length 
+  if deli_line > 0 
+    puts "The line is currently: #{deli_line}"
+  else
+    puts "The line is currently empty."
+  end 
+end
+binding.pry 
+p
